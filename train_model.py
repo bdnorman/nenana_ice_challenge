86ac019ec749be4114bed98ee521ab37e6403073
bdnorman <berknorman18@gmail.com>
2025-03-29 11:24:27 -0700

    2025 updates

--- train_model.py
+++ train_model.py
@@ -4,15 +4,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 from statsmodels.tsa.ar_model import AutoReg
+from datetime import datetime, timedelta
 
 
+_PREDICTING_YEAR = 2025
+
+
+# Run this with 1 or 2 lag
+# TRAIN_COLUMNS = [
+#     "Latest March Ice Reading",
+#     "march_low_temp",
+#     "march_high_temp",
+#     # "Latest March Date",
+#     "feb_avg_temp",
+#     'march_avg_temp',
+#     "Latest Feb Ice Reading",
+#     # "Latest Feb Date",
+# ]
+
+# Run this lage of 3 for 3.17 day error
 TRAIN_COLUMNS = [
     "Latest March Ice Reading",
-    "feb_low_temp",
-    "feb_high_temp",
+    # "march_low_temp",
+    # "march_high_temp",
     "Latest March Date",
-    "feb_avg_temp",
+    # "feb_avg_temp",
+    'march_avg_temp',
+    "Latest Feb Ice Reading",
+    "Latest Feb Date",
 ]
+
+def convert_decimal_day_to_date(decimal_day):
+    # Assuming a non-leap year starting on January 1
+    start_date = datetime(year=_PREDICTING_YEAR-1, month=1, day=1)
+    
+    # Calculate the full date and time from the decimal day
+    full_date_time = start_date + timedelta(days=decimal_day - 1)  # Subtract 1 because January 1st is day 1, not day 0
+    
+    # Format the date and time into month-day hour-minute format
+    formatted_date_time = full_date_time.strftime("%B %d, %I:%M %p")
+    
+    return formatted_date_time
 
 
 def main():
@@ -59,37 +91,47 @@ def main():
         start=1, end=len(df_val), exog_oos=df_val[TRAIN_COLUMNS]
     )
 
+    # -1 to account for 2024
     mse = mean_squared_error(
-        df_val["Decimal Day of Year"].values[num_lags - 1 :],
-        y_preds[num_lags - 1 :].values,
+        df_val["Decimal Day of Year"].values[num_lags - 1 :][:-1],
+        y_preds[num_lags - 1 :].values[:-1],
     )
     print(f"Mean Squared Error: {mse:.2f}")
     mean_day_error = np.mean(
         abs(
-            df_val["Decimal Day of Year"].values[num_lags - 1 :]
-            - y_preds[num_lags - 1 :].values
+            df_val["Decimal Day of Year"].values[num_lags - 1 :][:-1]
+            - y_preds[num_lags - 1 :].values[:-1]
         )
     )
     print(f"Mean Day Error: {mean_day_error:.2f}")
 
-    plt.plot(
-        df_val["Year"].values[(num_lags - 1) :],
-        df_val["Decimal Day of Year"].values[num_lags - 1 :],
-        "b",
-    )
-    plt.plot(
-        df_val["Year"].values[(num_lags - 1) :], y_preds[num_lags - 1 :].values, "r"
-    )
-    plt.legend(
-        [
-            "Actual Melt",
-            "Predicted Melt",
-        ]
-    )
+    plt.figure()
+    plt.scatter(df_full["Latest March Ice Reading"].values[:-1], df_full["Decimal Day of Year"].values[:-1], color='b')
+    plt.xlabel('March Ice Thickness')
+    plt.ylabel('Decimal Day of Year')
+    plt.savefig('Ice Reading vs Decimal Day of Year.png')
+
+    plt.figure()
+    actual_years = df_val["Year"].values[(num_lags - 1) :]
+    actual_values = df_val["Decimal Day of Year"].values[num_lags - 1 :]
+    predicted_values = y_preds[num_lags - 1 :].values
+    error = abs(actual_values[:-1] - predicted_values[:-1])
+    # Calculate the 95% confidence interval
+    confidence_interval = 1.96 * np.std(error) / np.sqrt(len(error))
+    print(confidence_interval)
+    lower_bound = predicted_values - confidence_interval
+    upper_bound = predicted_values + confidence_interval
+    plt.fill_between(actual_years, lower_bound, upper_bound, color='r', alpha=0.2, label='95% Confidence Interval')
+    plt.plot(actual_years, actual_values, 'o-', color='b', label='Actual Melt')
+    plt.legend()
     plt.xlabel("Year")
     plt.ylabel("Decimal Day of Year")
-    plt.title(f"Mean Squared Error: {mse:.2f}, Mean Day Error: {mean_day_error:.2f}")
-    plt.savefig("auto_reg_results.png")
+    date_pred_year = convert_decimal_day_to_date(y_preds.values[-1])
+
+    plt.title(f"Mean Squared Error: {mse:.2f}, Mean Day Error: {mean_day_error:.2f}\n2024 prediction: {date_pred_year}")
+    for i, txt in enumerate(error):
+        plt.annotate(f"{txt:.2f}", (actual_years[i], predicted_values[i]))
+    plt.savefig("auto_reg_results_with_confidence_interval.png")
 
 
 if __name__ == "__main__":
